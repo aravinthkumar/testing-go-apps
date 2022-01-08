3c418753c23e63b83f6aa67903f85631c8ced9a3
Aravinth <aravinthkumar.panchatcharam@gmail.com>
2022-01-08 16:25:20 +0530

    wrote tests for greet and depart method

--- code/messages/messages.go
+++ code/messages/messages.go
@@ -2,10 +2,10 @@ package messages
 
 import "fmt"
 
-func Greet(name string, id int) string {
-	return fmt.Sprintf("Hello, %v\n your ID is %v\n", name, id)
+func Greet(name string) string {
+	return fmt.Sprintf("Hello, %v", name)
 }
 
 func Depart(name string) string {
-	return fmt.Sprintf("Bye, %v\n", name)
+	return fmt.Sprintf("Bye, %v", name)
 }
--- code/messages/messages_test.go
+++ code/messages/messages_test.go
@@ -1,10 +1,23 @@
+// messages is used since we are using whitebox testing ,
+//all the menbers of the messages package can be used
+// message_test used then member can be access only by importing the package.
 package messages
 
 import "testing"
 
+// Test is prefixed for Go Test runner to identity this function as test
 func TestGreet(T *testing.T) {
+	actual := Greet("Gophers")
+	expected := "Hello, Gophers"
+	if actual != expected {
+		T.Errorf("Greet didn't matched, actual: '%v' but expected: '%v' ", actual, expected)
+	}
 }
 
 func TestDepart(T *testing.T) {
-
+	actual := Depart("Gophers")
+	expected := "Bye, Gophers"
+	if actual != expected {
+		T.Errorf("Greet didn't matched, actual: '%v' but expected: '%v' ", actual, expected)
+	}
 }
