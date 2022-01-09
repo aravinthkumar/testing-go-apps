3bd45f44e65f3dc8daf1ec5eb92847ca365aa669
Aravinth <aravinthkumar.panchatcharam@gmail.com>
2022-01-09 13:29:15 +0530

    Included Table Driven Test example

--- code/messages/messages_test.go
+++ code/messages/messages_test.go
@@ -3,7 +3,9 @@
 // message_test used then member can be access only by importing the package.
 package messages
 
-import "testing"
+import (
+	"testing"
+)
 
 // Test is prefixed for Go Test runner to identity this function as test
 func TestGreet(T *testing.T) {
@@ -14,10 +16,32 @@ func TestGreet(T *testing.T) {
 	}
 }
 
-// func TestDepart(T *testing.T) {
-// 	actual := Depart("Gophers")
-// 	expected := "Bye, Gophers"
-// 	if actual != expected {
-// 		T.Errorf("Greet didn't matched, actual: '%v' but expected: '%v' ", actual, expected)
-// 	}
-// }
+func TestDepart(T *testing.T) {
+	actual := Depart("Gophers")
+	expected := "Bye, Gophers"
+	if actual != expected {
+		T.Errorf("Greet didn't matched, actual: '%v' but expected: '%v' ", actual, expected)
+	}
+}
+
+// Table Driven Tests
+func TestGreetForTableDrivenTest(T *testing.T) {
+
+	scenarios := []struct {
+		input  string
+		output string
+	}{
+		{input: "Gophers", output: "Hello, Gophers"},
+		{input: "", output: "Hello,   "},
+	}
+
+	for _, s := range scenarios {
+
+		result := Greet(s.input)
+
+		if result != s.output {
+			T.Errorf(" Greet didn't match, actual: '%v' but expected: '%v'", result, s.output)
+		}
+	}
+
+}
